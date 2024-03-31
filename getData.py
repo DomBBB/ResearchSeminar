3b1640e61d44678e3b1681ba2e8977cba91f1b65
DomBBB <dominik.buchegger@icloud.com>
2024-03-31 03:05:50 +0200

    included date

--- getData.py
+++ getData.py
@@ -12,18 +12,28 @@ ToDos:
 
 
 import pandas as pd
-
 import wrds
 conn = wrds.Connection()
 
-quarterly_data = conn.raw_sql("""select gvkey, risq, piq, atq, ltq, teqq, iptiq, bctq
-                        from comp_global.g_fundq
-                        where gvkey = '270979'""")
+quarterly_data_all = pd.DataFrame()
+annual_data_all = pd.DataFrame()
 
-annual_data = conn.raw_sql("""select gvkey, ris, pi, at, lt, teq, ipti, bct
-                        from comp_global.g_funda
-                        where gvkey = '270979'""")
+companies = pd.read_csv("Overview Companies.csv", sep=";")
+company_subset = companies[["GVKEY (Compustat)"]]
+for x, y in company_subset.iterrows():
 
-conn.list_tables(library="comp_global")
+    quarterly_data = conn.raw_sql(f"""select gvkey, datadate, risq, piq, atq, ltq, teqq, iptiq, bctq
+                            from comp_global.g_fundq
+                            where gvkey = '{y[0]}'""")
+
+    annual_data = conn.raw_sql(f"""select gvkey, datadate, ris, pi, at, lt, teq, ipti, bct
+                            from comp_global.g_funda
+                            where gvkey = '{y[0]}'""")
+
+    quarterly_data_all = pd.concat([quarterly_data_all, quarterly_data], ignore_index=True)
+    annual_data_all = pd.concat([annual_data_all, annual_data], ignore_index=True)
 
 conn.close()
+
+quarterly_data_all.to_csv("quarterly_data_all.csv", index=False)
+annual_data_all.to_csv("annual_data_all.csv", index=False)
